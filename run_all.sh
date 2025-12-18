9b64814f76b28f2b20d1b05a21bb224922735ae8
Yu <q13426724541@163.com>
2025-12-18 20:39:16 +0800

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,24 @@
+#!/bin/bash
+
+echo "开始下载数据..."
 python 1.yahoo_data_download.py
+
+
+echo "生成视图..."
 python 7.create_view.py
+
+echo "开始质量检查..."
 python 2.QC.py
+
+echo "开始check..."
 python 3.check.py
+
+echo "正在迁移到数据库..."
 python 4.migrate_to_postgres.py
+
+echo "设置规则..."
 python 5.rag_setup.py
+
+echo "调用大模型..."
 python 6.query_llama_postgres.py
+
